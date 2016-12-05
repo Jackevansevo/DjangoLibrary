0d73d1dc05bdcc37429925bf28c9ae4be0e36885
Jack Evans <jwe@tiger-computing.co.uk>
2016-12-05 10:57:31 +0000

    Tweaks imports to use isbnlib namespace instead of explicity importing a bazillion things

--- books/forms.py
+++ books/forms.py
@@ -1,20 +1,21 @@
 from django import forms
 
-from .isbn import has_english_identifier, is_isbn13, meta, to_isbn13
-from .models import Review
+from books.models import Review
+
+import books.isbn as isbnlib
 
 
 class BookCreateForm(forms.Form):
     isbn = forms.CharField(max_length=17)
 
     def clean_isbn(self):
-        data = to_isbn13(self.cleaned_data['isbn'])
-        if not is_isbn13(data):
+        data = isbnlib.to_isbn13(self.cleaned_data['isbn'])
+        if not isbnlib.is_isbn13(data):
             raise forms.ValidationError('ISBN Number was Invalid')
-        if not has_english_identifier(data):
+        if not isbnlib.has_english_identifier(data):
             error_msg = 'ISBN Contains a non English-language identifier'
             raise forms.ValidationError(error_msg)
-        if not meta(data):
+        if not isbnlib.meta(data):
             raise forms.ValidationError('Book Meta-data not found')
         return data
 
