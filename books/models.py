81215996427362fcf66f9da9f8ae8b1825d9013e
Jack Evans <jwe@tiger-computing.co.uk>
2016-12-06 09:53:31 +0000

    Move methods into model managers

--- books/models.py
+++ books/models.py
@@ -101,12 +101,39 @@ class Genre(models.Model):
         return self.name
 
 
+class BookManager(models.Manager):
+
+    def create_book_from_metadata(self, isbn):
+        book, created = self.get_or_create(isbn=isbn)
+        if created:
+            meta_info = meta(isbn)
+            book.title = capwords(meta_info.get('title', ''))
+            book.subtitle = capwords(meta_info.get('subtitle', ''))
+            book.img = meta_info.get('img')
+            # Save the Book
+            book.save()
+
+            # Add the ManyToMany related authors
+            for name in meta_info.get('authors', []):
+                name = capwords(name)
+                author, created = Author.objects.get_or_create(name=name)
+                book.authors.add(author)
+
+            for name in meta_info.get('categories', []):
+                name = capwords(name)
+                genre, created = Genre.objects.get_or_create(name=name)
+                book.genres.add(genre)
+        return book
+
+
 class Book(TimeStampedModel):
     isbn = models.CharField(max_length=13, primary_key=True)
     title = models.CharField(max_length=200, db_index=True, unique=True)
     subtitle = models.CharField(max_length=200, blank=True)
     img = models.URLField(default='http://placehold.it/150x225')
     slug = models.SlugField(max_length=200)
+
+    objects = BookManager()  # Book specific manager
 
     authors = models.ManyToManyField('Author', related_name='books')
     genres = models.ManyToManyField('Genre', related_name='books')
@@ -215,33 +242,3 @@ class Review(models.Model):
 
     def __str__(self):
         return str(self.rating)
-
-
-def create_book(isbn):
-    book, created = Book.objects.get_or_create(isbn=isbn)
-    if created:
-        # If the book is new, populate it's fields
-        meta_info = meta(isbn)
-        book.title = capwords(meta_info.get('title', ''))
-        book.subtitle = capwords(meta_info.get('subtitle', ''))
-        book.img = meta_info.get('img')
-        # Save the Book
-        book.save()
-
-        # Add the ManyToMany related authors
-        for name in meta_info.get('authors', []):
-            name = capwords(name)
-            author, created = Author.objects.get_or_create(name=name)
-            book.authors.add(author)
-
-        for name in meta_info.get('categories', []):
-            name = capwords(name)
-            genre, created = Genre.objects.get_or_create(name=name)
-            book.genres.add(genre)
-    return book
-
-
-def add_book_copy(isbn):
-    book = create_book(isbn)
-    BookCopy.objects.create(book=book)
-    return book
