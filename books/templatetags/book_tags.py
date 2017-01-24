716cfbb7660d786bdcddbcab92266150e3b39f76
Jack Evans <jwe@tiger-computing.co.uk>
2017-01-24 17:23:32 +0000

    modified templatetags syntax in keeping with Django docs

--- books/templatetags/book_tags.py
+++ books/templatetags/book_tags.py
@@ -4,7 +4,7 @@ from django.utils.safestring import mark_safe
 register = Library()
 
 
-@register.filter(name="getReviewStars")
-def getReviewStars(value):
-    star = "<i class='fa gold fa-star' aria-hidden='true'></i>"
+@register.filter(name="prettystars")
+def get_review_stars(value):
+    star = "<i class='fa fa-star text-warning' aria-hidden='true'></i>"
     return mark_safe(star*(int(round(value))))
