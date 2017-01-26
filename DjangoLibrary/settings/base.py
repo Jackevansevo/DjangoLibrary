b9bef13f9c9e0b27a8c61e43dfd8b5afb5e7c9bf
Jack Evans <jwe@tiger-computing.co.uk>
2017-01-26 17:25:11 +0000

    Add humanize to installed apps

--- DjangoLibrary/settings/base.py
+++ DjangoLibrary/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'django.contrib.humanize',
     'books.apps.BooksConfig',
     'widget_tweaks'
 ]
