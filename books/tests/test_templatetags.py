f70a2d606fee366da2397880e0a9650b1d009992
Jack Evans <jack@evans.gb.net>
2017-01-28 21:55:45 +0000

    updated failing test

--- books/tests/test_templatetags.py
+++ books/tests/test_templatetags.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
-from books.templatetags.book_tags import getReviewStars
+from books.templatetags.book_tags import review_stars
 
 
 class TestGetReviewStars(TestCase):
 
     def test_review_stars(self):
-        star_text = "<i class='fa gold fa-star' aria-hidden='true'></i>"
-        expected = star_text * 2
+        star = "<i class='fa fa-star text-warning' aria-hidden='true'></i>"
+        expected = star * 2
         # Ensure the template tag results the same resutl as expected
-        self.assertEqual(getReviewStars(2.4), expected)
+        self.assertEqual(review_stars(2.4), expected)
