33b5875a16618b2da66c33b0882a55640cad5d06
Jack Evans <jack@evans.gb.net>
2017-01-30 13:47:09 +0000

    Replaced silly placeholder with something useful, now auto submits forms when ISBN-like string is pasted into the input box

--- books/static/books/book-create.js
+++ books/static/books/book-create.js
@@ -1,1 +1,12 @@
-console.log('wew');
+/**
+ * Automatically submit the quick create form if user pastes in string
+ * resembling an ISBN
+ */
+$('#id_isbn').on('paste', function() {
+    setTimeout(function() {
+        var input = $('#id_isbn').val();
+        if (input.length == 10 || input.length == 13) {
+            $('#ISBNFormSubmit').click();
+        }
+    }, 0);
+});
